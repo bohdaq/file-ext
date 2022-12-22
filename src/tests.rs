56d0f8ef54ccf88805138095d153ffbc34906060
Bohdan Tsap <bohdan.tsap@tutanota.com>
2022-12-22 11:40:35 +0200

    wip

--- src/tests.rs
+++ src/tests.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use crate::FileExt;
 
 #[test]
@@ -12,4 +13,12 @@ fn not_symlink_check() {
     let path = "test/index.html";
     let is_symlink = FileExt::is_symlink(path).unwrap();
     assert!(!is_symlink);
+}
+
+#[test]
+fn link_points_to() {
+    let path = "test/index_rewrite";
+    let path_buff = fs::read_link(path).unwrap();
+    let points_to = path_buff.as_path().to_str().unwrap();
+    assert_eq!("index.html", points_to);
 }
