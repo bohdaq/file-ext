7d494dd29b43df318859dee3faf44b9012e56579
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-01-11 07:56:09 +0200

    wip

--- src/symlink_ext_impl/mod.rs
+++ src/symlink_ext_impl/mod.rs
@@ -47,20 +47,20 @@ impl SymlinkExtImpl {
     pub fn create_symlink(symlink_path: &str, symlink_name: &str, symlink_points_to: &str) -> Result<(), String> {
         //check if there is already a file where symlink is going to be created
         let path_to_symlink_included = [symlink_path, symlink_name].join("");
-        let does_file_exist = FileExt::does_file_exist(&path_to_symlink_included);
+        let does_file_exist = FileExtImpl::does_file_exist(&path_to_symlink_included);
         if does_file_exist {
             let message = format!("There is a file on a given path: {}", &path_to_symlink_included);
             return Err(message)
         }
-        let does_directory_exist = FileExt::does_directory_exist(&path_to_symlink_included);
+        let does_directory_exist = DirectoryExtImpl::does_directory_exist(&path_to_symlink_included);
         if does_directory_exist {
             let message = format!("There is a directory on a given path: {}", &path_to_symlink_included);
             return Err(message)
         }
 
         //check if there is a file or directory for symlink to be created
-        let does_file_exist = FileExt::does_file_exist(symlink_points_to);
-        let does_directory_exist = FileExt::does_directory_exist(symlink_points_to);
+        let does_file_exist = FileExtImpl::does_file_exist(symlink_points_to);
+        let does_directory_exist = DirectoryExtImpl::does_directory_exist(symlink_points_to);
 
         if !does_file_exist && !does_directory_exist   {
             let message = format!("There is no file or directory for symlink to be created: {}", symlink_points_to);
