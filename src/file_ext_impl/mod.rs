22fb9134f6c411d642d1fffde4286498a652c88c
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-01-11 02:12:13 +0200

    wip

---
+++ src/file_ext_impl/mod.rs
@@ -0,0 +1,57 @@
+use std::fs::File;
+use std::io::{BufReader, Read, Seek, SeekFrom};
+
+pub struct FileExtImpl;
+
+impl FileExtImpl {
+    pub fn read_file(filepath: &str) -> Result<Vec<u8>, String> {
+
+        let mut file_content = Vec::new();
+        let boxed_open = File::open(filepath);
+        if boxed_open.is_err() {
+            let error_msg = boxed_open.err().unwrap();
+            let error = format!("<p>Unable to open file: {}</p> <p>error: {}</p>", filepath, error_msg);
+            return Err(error)
+        } else {
+            let mut file = boxed_open.unwrap();
+            let boxed_read= file.read_to_end(&mut file_content);
+            if boxed_read.is_err() {
+                let error_msg = boxed_read.err().unwrap();
+                let error = format!("<p>Unable to read file: {}</p> <p>error: {}</p>", filepath, error_msg);
+                return Err(error)
+            }
+        }
+        Ok(file_content)
+    }
+
+    pub fn read_file_partially(filepath: &str, start: u64, end: u64) -> Result<Vec<u8>, String> {
+        let mut file_content = Vec::new();
+
+        let buff_length = (end - start) + 1;
+        let boxed_open = File::open(filepath);
+        if boxed_open.is_err() {
+            let error_msg = boxed_open.err().unwrap();
+            let error = format!("<p>Unable to open file: {}</p> <p>error: {}</p>", filepath, error_msg);
+            return Err(error)
+        }
+
+        let file = boxed_open.unwrap();
+        let mut reader = BufReader::new(file);
+
+        let boxed_seek = reader.seek(SeekFrom::Start(start));
+        if boxed_seek.is_ok() {
+            let boxed_read = reader.take(buff_length).read_to_end(&mut file_content);
+            if boxed_read.is_err() {
+                let error_msg = boxed_read.err().unwrap().to_string();
+                let error = format!("<p>Unable to read file: {}</p> <p>error: {}</p>", filepath, error_msg);
+                return Err(error)
+            }
+        } else {
+            let error_msg = boxed_seek.err().unwrap().to_string();
+            let error = format!("<p>Unable to seek file: {}</p> <p>error: {}</p>", filepath, error_msg);
+            return Err(error)
+        }
+
+        Ok(file_content)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
 use std::{env, fs};
 use std::fs::{File, OpenOptions};
-use std::io::{BufReader, Read, Seek, SeekFrom, Write};
+use std::io::{Seek, SeekFrom, Write};
 use std::path::Path;
 use std::process::Command;
 use crate::date_time_ext::DateTimeExt;
+use crate::file_ext_impl::FileExtImpl;
 use crate::symbol::SYMBOL;
 
 #[cfg(test)]
 mod tests;
 mod date_time_ext;
 mod symbol;
+mod file_ext_impl;
 
 pub struct FileExt;
 
@@ -32,34 +34,7 @@ impl FileExt {
     /// }
     /// ```
     pub fn read_file_partially(filepath: &str, start: u64, end: u64) -> Result<Vec<u8>, String> {
-        let mut file_content = Vec::new();
-
-        let buff_length = (end - start) + 1;
-        let boxed_open = File::open(filepath);
-        if boxed_open.is_err() {
-            let error_msg = boxed_open.err().unwrap();
-            let error = format!("<p>Unable to open file: {}</p> <p>error: {}</p>", filepath, error_msg);
-            return Err(error)
-        }
-
-        let file = boxed_open.unwrap();
-        let mut reader = BufReader::new(file);
-
-        let boxed_seek = reader.seek(SeekFrom::Start(start));
-        if boxed_seek.is_ok() {
-            let boxed_read = reader.take(buff_length).read_to_end(&mut file_content);
-            if boxed_read.is_err() {
-                let error_msg = boxed_read.err().unwrap().to_string();
-                let error = format!("<p>Unable to read file: {}</p> <p>error: {}</p>", filepath, error_msg);
-                return Err(error)
-            }
-        } else {
-            let error_msg = boxed_seek.err().unwrap().to_string();
-            let error = format!("<p>Unable to seek file: {}</p> <p>error: {}</p>", filepath, error_msg);
-            return Err(error)
-        }
-
-        Ok(file_content)
+        FileExtImpl::read_file_partially(filepath, start, end)
     }
 
     /// Returns file content
@@ -81,23 +56,7 @@ impl FileExt {
     ///  }
     /// ```
     pub fn read_file(filepath: &str) -> Result<Vec<u8>, String> {
-
-        let mut file_content = Vec::new();
-        let boxed_open = File::open(filepath);
-        if boxed_open.is_err() {
-            let error_msg = boxed_open.err().unwrap();
-            let error = format!("<p>Unable to open file: {}</p> <p>error: {}</p>", filepath, error_msg);
-            return Err(error)
-        } else {
-            let mut file = boxed_open.unwrap();
-            let boxed_read= file.read_to_end(&mut file_content);
-            if boxed_read.is_err() {
-                let error_msg = boxed_read.err().unwrap();
-                let error = format!("<p>Unable to read file: {}</p> <p>error: {}</p>", filepath, error_msg);
-                return Err(error)
-            }
-        }
-        Ok(file_content)
+        FileExtImpl::read_file(filepath)
     }
 
     /// Returns file modification timestamp as nanoseconds in Unix epoch
