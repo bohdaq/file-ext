80dcfc0242980fbbb07db01fcd6bf70f6bf3fce3
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-01-12 08:08:46 +0200

    wip

--- src/date_time_ext/tests.rs
+++ src/date_time_ext/tests.rs
@@ -15,16 +15,3 @@ fn now_as_nanos() {
     let nanos = DateTimeExt::_now_unix_epoch_nanos();
     assert_ne!(nanos, 0);
 }
-
-#[test]
-fn new_directory_create_delete() {
-    let path = "new_directory";
-
-    let boxed_create = FileExt::create_directory(path);
-    assert!(boxed_create.is_ok());
-
-    assert!(FileExt::does_directory_exist(path));
-
-    let boxed_delete = FileExt::delete_directory(path);
-    assert!(boxed_delete.is_ok());
-}
