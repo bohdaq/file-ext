faffae5422457eb0373d4ca33e790e9aaa805963
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-01-15 06:54:01 +0200

    wip

--- src/symlink_ext_impl/tests.rs
+++ src/symlink_ext_impl/tests.rs
@@ -123,14 +123,10 @@ fn resolve_symlink_path() {
     let base_dir = "/home/someuser/folder/subfolder/subsubfolder";
     let symlink_points_to = "../../subfolder2/subsubfolder2";
 
-   // FileExt::create_file("resolve.out").unwrap();
-
     let expected_path = "/home/someuser/folder/subfolder2/subsubfolder2";
     let actual_path = resolve_path(base_dir, symlink_points_to).unwrap();
 
     assert_eq!(expected_path, actual_path);
-   // FileExt::delete_file("resolve.out").unwrap();
-
 }
 
 #[test]
@@ -138,14 +134,10 @@ fn resolve_symlink_path_subdirectory() {
     let base_dir = "/home/someuser/folder/subfolder/subsubfolder";
     let symlink_points_to = "subsubsubfolder/subsubsubsubfolder";
 
-    // FileExt::create_file("resolve.out").unwrap();
-
     let expected_path = "/home/someuser/folder/subfolder/subsubfolder/subsubsubfolder/subsubsubsubfolder";
     let actual_path = resolve_path(base_dir, symlink_points_to).unwrap();
 
     assert_eq!(expected_path, actual_path);
-    // FileExt::delete_file("resolve.out").unwrap();
-
 }
 
 #[test]
@@ -153,19 +145,13 @@ fn resolve_symlink_path_root() {
     let base_dir = "/home/someuser/folder/subfolder/subsubfolder";
     let symlink_points_to = "/tmp/folder";
 
-    // FileExt::create_file("resolve.out").unwrap();
-
     let expected_path = "/tmp/folder";
     let actual_path = resolve_path(base_dir, symlink_points_to).unwrap();
 
     assert_eq!(expected_path, actual_path);
-    // FileExt::delete_file("resolve.out").unwrap();
-
 }
 
 fn resolve_path(base_dir: &str, symlink_points_to: &str) -> Result<String, String> {
-    // FileExt::write_file("resolve.out", format!("\n\n base dir:{}\n symlink_points_to: {}\n", base_dir, symlink_points_to).as_bytes()).unwrap();
-
     if symlink_points_to.starts_with(SYMBOL.slash) {
         return Ok(symlink_points_to.to_string())
     }
