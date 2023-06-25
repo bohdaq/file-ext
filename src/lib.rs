6d3b6d269e4895f1d61d1c43f4f8a5aa34013bba
Bohdan Tsap <bohdan.tsap@tutanota.com>
2023-06-25 06:20:09 +0300

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -801,6 +801,32 @@ impl FileExt {
         FileExtImpl::copy_file_with_callbacks(from, to, block_size, progress_callback, cancel_callback)
     }
 
+    /// Copies file block by block starting from specific byte. If block size is None it is set to 100kb.
+    /// Calls the progress callback at the beginning of the block copy.
+    /// Calls the cancel callback at the end of the block copy.
+    ///```
+    /// use file_ext::FileExt;
+    /// #[test]
+    /// fn copy_file_with_callback_and_block_size_starting_from_byte() {
+    ///     let block_size : u64 = 1000000;
+    ///     let pwd = FileExt::working_directory().unwrap();
+    ///     let mut label = "".to_string();
+    ///     let progress_callback = |start, end, total| { label = format!("copying block {}-{} of {} bytes", start, end, total).to_string(); };
+    ///     let cancel_callback = |_start, _end, _total| { false };
+    ///     let starting_byte = 4;
+    ///     FileExt::copy_file_with_callbacks_starting_from_byte(
+    ///         vec![pwd.as_str(), "LICENSE"],
+    ///         vec![pwd.as_str(), "LICENSE_copy4"],
+    ///         starting_byte,
+    ///         Some(block_size),
+    ///         progress_callback,
+    ///         cancel_callback
+    ///     ).unwrap();
+    ///
+    ///     let path = FileExt::build_path(vec![pwd.as_str(), "LICENSE_copy4"].as_slice());
+    ///     FileExt::delete_file(path.as_str()).unwrap();
+    /// }
+    ///```
     pub fn copy_file_with_callbacks_starting_from_byte
     <F: FnMut(u64, u64, u64), C: FnMut(u64, u64, u64) -> bool>
     (
